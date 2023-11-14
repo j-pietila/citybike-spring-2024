9d01be7450dfa9d39e09baa4cae6bb1b8291b977
Joonas Pietilä <joonas.pietila@gmail.com>
2023-11-14 19:54:24 +0200

    Frontend #14 - Front-end production build with build configs. Fixed rest of the TypeScript errors.

--- front-end/docker/dev/entrypoint.sh
+++ front-end/docker/dev/entrypoint.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Remove possible production built front-end from
+# web-root volume to let Nginx serve dev front-end container
+rm  /run/web/index.html
+rm -rf /run/web/assets
+
+
 cd /run/src/app
 
 yarn install
